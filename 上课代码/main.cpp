aba8c01baf00133d7a68f6cb3b68ae1de3db809b
20181102931 <18647197975@163.com>
2019-09-24 16:21:45 +0800

    Initial Commit

---
+++ 上课代码/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  上课代码
+//
+//  Created by s20181102931 on 2019/9/24.
+//  Copyright © 2019 s20181102931. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
